53c184921c9a7e5652980221552283bd040fddda
dooory <78720364+dooory@users.noreply.github.com>
2025-06-04 17:09:01 +1200

    Edited init.lua

--- arch-zenbook/.config/nvim/init.lua
+++ arch-zenbook/.config/nvim/init.lua
@@ -248,7 +248,11 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
+  {
+    'vyfor/cord.nvim',
+    build = ':Cord update',
+    -- opts = {}
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
