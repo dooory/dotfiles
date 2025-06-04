f2e21b7a52fa8d5eb69ac9323bbfbfb95b09fbda
dooory <78720364+dooory@users.noreply.github.com>
2025-06-04 10:38:58 +0000

    Copied arch-zenbook dotfiles to archPC

---
+++ archPC/bash-scripts/autostart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+hyprpaper &
+waybar &
+kitty &
+discord --ozone-platform=wayland --enable-features=UseOzonePlatform &
+spotify --ozone-platform=wayland --enable-features=UseOzonePlatform &
+
