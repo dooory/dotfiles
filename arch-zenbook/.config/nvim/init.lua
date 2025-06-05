6f9c6b0766ebef471fea12893b31480c19883993
dooory <78720364+dooory@users.noreply.github.com>
2025-06-05 16:34:28 +1200

    Synced PC nvim config + added cord plugin

--- arch-zenbook/.config/nvim/init.lua
+++ arch-zenbook/.config/nvim/init.lua
@@ -34,7 +34,8 @@ What is Kickstart?
 
     If you don't know anything about Lua, I recommend taking some time to read through
     a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
+      - https://learnxinyminutes.com/docs/lua/,
+    
 
     After understanding a bit more about Lua, you can use `:help lua-guide` as a
     reference for how Neovim integrates Lua.
@@ -248,11 +249,6 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-  {
-    'vyfor/cord.nvim',
-    build = ':Cord update',
-    -- opts = {}
-  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -526,6 +522,40 @@ require('lazy').setup({
       --    That is to say, every time a new file is opened that is associated with
       --    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
       --    function will be executed to configure the current buffer
+      --
+
+      vim.api.nvim_create_autocmd({ 'FileType' }, {
+        pattern = 'css,eruby,html,htmldjango,javascriptreact,less,pug,sass,scss,typescriptreact',
+        callback = function()
+          vim.lsp.start {
+            cmd = { 'emmet-language-server', '--stdio' },
+            root_dir = vim.fs.dirname(vim.fs.find({ '.git' }, { upward = true })[1]),
+            -- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
+            -- **Note:** only the options listed in the table are supported.
+            init_options = {
+              ---@type table<string, string>
+              includeLanguages = {},
+              --- @type string[]
+              excludeLanguages = {},
+              --- @type string[]
+              extensionsPath = {},
+              --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+              preferences = {},
+              --- @type boolean Defaults to `true`
+              showAbbreviationSuggestions = true,
+              --- @type "always" | "never" Defaults to `"always"`
+              showExpandedAbbreviation = 'always',
+              --- @type boolean Defaults to `false`
+              showSuggestionsAsSnippets = false,
+              --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+              syntaxProfiles = {},
+              --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+              variables = {},
+            },
+          }
+        end,
+      })
+
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
@@ -678,7 +708,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        -- rcss-lsp cssls
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -988,8 +1018,8 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
-  --
+  { import = 'custom.plugins' },
+
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
   -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
--- arch-zenbook/.config/nvim/lua/custom/plugins/init.lua
+++ arch-zenbook/.config/nvim/lua/custom/plugins/init.lua
@@ -2,4 +2,63 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'Jezda1337/nvim-html-css',
+    --dependencies = { "hrsh7th/nvim-cmp", "nvim-treesitter/nvim-treesitter" }, -- Use this if you're using nvim-cmp
+    dependencies = { 'saghen/blink.cmp', 'nvim-treesitter/nvim-treesitter' }, -- Use this if you're using blink.cmp
+    opts = {
+      enable_on = { -- Example file types
+        'html',
+        'htmldjango',
+        'tsx',
+        'jsx',
+        'erb',
+        'svelte',
+        'vue',
+        'blade',
+        'php',
+        'templ',
+        'astro',
+      },
+      handlers = {
+        definition = {
+          bind = 'gd',
+        },
+        hover = {
+          bind = 'K',
+          wrap = true,
+          border = 'none',
+          position = 'cursor',
+        },
+      },
+      documentation = {
+        auto_show = true,
+      },
+      style_sheets = {
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.3/css/bulma.min.css',
+        './index.css', -- `./` refers to the current working directory.
+      },
+    },
+  },
+  {
+    'L3MON4D3/LuaSnip',
+    -- follow latest release.
+    version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = 'make install_jsregexp',
+  },
+  {
+    'vyfor/cord.nvim',
+    build = ':Cord update',
+    opts = {
+      hooks = {
+        post_activity = function(opts, activity)
+          local version = vim.version()
+          activity.assets.small_text = string.format('Kickstart Neovim %s.%s.%s', version.major, version.minor, version.patch)
+        end,
+      },
+    },
+  },
+}
