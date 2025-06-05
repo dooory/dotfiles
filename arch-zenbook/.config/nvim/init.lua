5381db96fcd454a1f5a54b36170a75bf73991f10
dooory <78720364+dooory@users.noreply.github.com>
2025-06-05 18:23:15 +1200

    Edited neovim config
    Made line wrapping happen after words instead of within them

--- arch-zenbook/.config/nvim/init.lua
+++ arch-zenbook/.config/nvim/init.lua
@@ -110,7 +110,7 @@ vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = true
-
+vim.o.linebreak = true
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
