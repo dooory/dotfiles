ef64c6da67e2c1bd6697ee8b5a28afc5c2738068
dooory <78720364+dooory@users.noreply.github.com>
2025-06-09 23:11:48 +1200

    nvim: setting color scheme to load when required

--- arch-zenbook/.config/nvim/lua/plugins/appearance.lua
+++ arch-zenbook/.config/nvim/lua/plugins/appearance.lua
@@ -1,4 +1,17 @@
 return {
-	{ "olimorris/onedarkpro.nvim", priority = 1000 },
-	{ "nvim-tree/nvim-web-devicons", opts = {} },
+  {
+    "olimorris/onedarkpro.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("onedarkpro").setup({
+        colors = {}, -- override default colors if needed
+        options = {
+          transparency = false,
+        },
+      })
+      vim.cmd([[colorscheme onedark]]) -- You can also use "onelight" or "onelightpro"
+    end,
+  },
+  { "nvim-tree/nvim-web-devicons", opts = {} },
 }
