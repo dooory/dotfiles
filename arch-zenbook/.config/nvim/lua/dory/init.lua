b2cfdc4158ca964448792415bdd6e61b2e977d3c
dooory <78720364+dooory@users.noreply.github.com>
2025-06-09 23:22:04 +1200

    nvim: added telescope mappings + comments for dory/init.lua

--- arch-zenbook/.config/nvim/lua/dory/init.lua
+++ arch-zenbook/.config/nvim/lua/dory/init.lua
@@ -1,6 +1,11 @@
---// Loading lazy vim
+--// Loading personal neovim remappings
 require("dory.remap")
 
+--// Loading lazy vim
 require("config.lazy")
 
+--// Loading personal plugin specific remappings
+require("dory.remap-plugins")
+
+--// Loading personal appearance config
 require("dory.appearance")
