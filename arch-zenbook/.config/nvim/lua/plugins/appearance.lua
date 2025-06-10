7333b16ffedc521ab26ef2a5f38e327aea86c935
dooory <78720364+dooory@users.noreply.github.com>
2025-06-10 17:16:39 +1200

    nvim: changed to kanagawa-dragon theme

--- arch-zenbook/.config/nvim/lua/plugins/appearance.lua
+++ arch-zenbook/.config/nvim/lua/plugins/appearance.lua
@@ -1,17 +1,12 @@
 return {
-  {
-    "olimorris/onedarkpro.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("onedarkpro").setup({
-        colors = {}, -- override default colors if needed
-        options = {
-          transparency = false,
-        },
-      })
-      vim.cmd([[colorscheme onedark]]) -- You can also use "onelight" or "onelightpro"
-    end,
-  },
-  { "nvim-tree/nvim-web-devicons", opts = {} },
+	{
+		"rebelot/kanagawa.nvim",
+		priority = 1000,
+		opts = {
+		},
+		config = function()
+			vim.cmd('colorscheme kanagawa-dragon')
+		end,
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
 }
