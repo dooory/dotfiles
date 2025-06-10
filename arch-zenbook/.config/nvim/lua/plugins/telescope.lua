db63692d968aa85d6772bdcb9207c6a424064fd1
dooory <78720364+dooory@users.noreply.github.com>
2025-06-10 16:09:52 +1200

    nvim: added telescope-fzf-native.nvim to telescope.nvim

--- arch-zenbook/.config/nvim/lua/plugins/telescope.lua
+++ arch-zenbook/.config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,8 @@
 return {
 	'nvim-telescope/telescope.nvim',
-	tag = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+	tag = '0.1.8',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' }
+	},
 }
